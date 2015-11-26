8c7b2d0fc73236b6c3da4003bd9b14eb326ac612
youngdae jun <ydse@rankwave.net>
2015-11-26 13:51:16 +0900

    version 2.1.1 Add

--- eclipse/sdk/src/com/rankwave/connect/sdk/Connect.java
+++ eclipse/sdk/src/com/rankwave/connect/sdk/Connect.java
@@ -23,7 +23,7 @@ public final class Connect {
 	public static final String PROPERTY_NOTIFICATION_LARGE_ICON = "com.rankwave.notification.large_icon";
 	public static final String PROPERTY_NOTIFICATION_SMALL_ICON = "com.rankwave.notification.small_icon";
 	
-	public static final String SDK_VERISON = "2.1.0";
+	public static final String SDK_VERISON = "2.1.1";
 	public static final String INTENT_PUSH_PAYLOAD = "com.rankwave.connect.sdk.pushpayload";
 	public static final String INTENT_PUSH_SEQ = "com.rankwave.connect.sdk.pushseq";
 	public static final String INTENT_PUSH_CMN = "com.rankwave.connect.sdk.pushcmn";
